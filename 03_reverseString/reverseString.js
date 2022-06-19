d7db021b009c1e483c7265a61af800ac65777b4a
Oggup <edifoo@gmail.com>
2022-06-19 12:22:32 -0700

    finished excercise 3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  //turn it into an array, reverse the array, join it all into a string
+  return string.split("").reverse().join("")
 
+
+  // create empty string> for loop starting at last letter of string> goes down till 0> new string variable is last letter plus next letter going down
+//   let newString = "";
+//   for (i = string.length - 1; i >= 0; i--) {
+//     newString += string[i];
+//   }
+//   return newString;
 };
 
 // Do not edit below this line
