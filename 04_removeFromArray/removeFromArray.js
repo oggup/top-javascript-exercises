dcbfc8ead4dfb1ed92b47de8c9fdfa5cb6b50f6c
Oggup <edifoo@gmail.com>
2022-06-19 13:57:05 -0700

    excercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...thingsToRemove) {
+    array = array.filter((element)=> !thingsToRemove.includes(element))
+    return array
 };
+
+//filter through the array returning an array that does not include the elements of thingsToRemove
 
 // Do not edit below this line
 module.exports = removeFromArray;
