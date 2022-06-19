11729360de796a9e987c926072c12b87436103c2
Oggup <edifoo@gmail.com>
2022-06-18 23:48:00 -0700

    completed initial 2 excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, timesRepeated) {
+  let string = "";
+  if (timesRepeated <0){
+      return "ERROR"
+  }
+  for (i = 0; i < timesRepeated; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
