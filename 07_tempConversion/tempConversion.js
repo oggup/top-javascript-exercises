a6082e0bdd7b3fa9826a2b56367197420c9cc367
Oggup <edifoo@gmail.com>
2022-06-19 21:52:59 -0700

    excercise 06 convert temperatures complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5 / 9);
+  celsius = Number(celsius.toFixed(1));
 
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let farenheit = (celsius * (9 / 5) )+ 32;
+  farenheit = Number(farenheit.toFixed(1))
+  return farenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
