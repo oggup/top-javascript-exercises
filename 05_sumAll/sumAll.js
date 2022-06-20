6e80e25ac2fca923fee8d8063939d06b6ba75bac
Oggup <edifoo@gmail.com>
2022-06-19 20:00:32 -0700

    excercise 5 sumAll complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  let sum = 0;
+  if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0) {
+    return "ERROR";
+  }
+  if (min > max) {
+    const newMax = min;
+    min = max;
+    max = newMax;
+  }
+  for (i = min; i <= max; i++) {
+    sum += i;
+  }
+  console.log(min, max);
+  return sum;
 };
 
 // Do not edit below this line
