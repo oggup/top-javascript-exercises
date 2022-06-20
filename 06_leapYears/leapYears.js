f9e6495115759a09138da4848d74c0c5aa0c31a9
Oggup <edifoo@gmail.com>
2022-06-19 21:31:31 -0700

    excerices 06 leapYears completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  //using if statement
+  //   if (year % 4 === 0 && (year % 100 != 0 || year % 400 === 0)) {
+  //     return true;
+  //   }
+  //   return false;
+  //using ternery conditional
+  return (result =
+    year % 4 === 0 && (year % 100 != 0 || year % 400 === 0) ? true : false);
 };
 
 // Do not edit below this line
