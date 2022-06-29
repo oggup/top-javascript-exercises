36ef78090face06cae2c92ee8135b4d9f4a13ac5
Oggup <edifoo@gmail.com>
2022-06-29 12:14:07 -0700

    working on calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,22 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  array.reduce((total,previous)=>{ 
+    total + previous
+  }, 0)
 };
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +25,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
