c8d38cd5cb9ee3dd855aeb00ff00306257451b5b
Oggup <edifoo@gmail.com>
2022-06-29 18:03:46 -0700

    palindrome excercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+//   let newString = string.split().reverse().join();
+//   if (string.toLowerCase() === newString.toLowerCase()) return true;
 
+let reg= /[\W_]/g
+let smallString = string.toLowerCase().replace(reg, '');
+let reversed = smallString.split('').reverse().join("");
+if(reversed === smallString)return true;
+return false;
 };
 
 // Do not edit below this line
