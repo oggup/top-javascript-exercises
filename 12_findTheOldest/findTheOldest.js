caefc54efa7c9926ca6261bf9e554cd8962da6fb
Oggup <edifoo@gmail.com>
2022-06-30 16:27:06 -0700

    completed findtheoldest excercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+ return array.reduce((oldest, currentPerson) => {
+    if(!oldest.yearOfDeath || !currentPerson.yearOfDeath){
+        oldest.yearOfDeath= new Date().getFullYear();
+        currentPerson.yearOfDeath= new Date().getFullYear()
+    }
+    
+    let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    let currentPersonAge =
+      currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    return oldestAge < currentPersonAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
