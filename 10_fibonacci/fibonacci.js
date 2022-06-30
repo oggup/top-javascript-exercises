4cbb0d0427010748247f1ca561425e0a526b8328
Oggup <edifoo@gmail.com>
2022-06-29 18:31:34 -0700

    fibonaci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  for (i = 1; i < n; i++) {
+    let temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
